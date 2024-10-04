f1c0c340fa4b79f90b86982acdd7fe7e9aee3277
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-10-04 16:13:43 +0100

    Create Bottom tab navigation.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import FoodCategoryList from "./screens/FoodCategoryList";
 import FavoritesList from "./screens/FavoritesList";
+import { NavigationContainer } from "@react-navigation/native";
 
+// Create Navigators
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
@@ -19,18 +20,8 @@ function Tabs() {
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Tabs />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
